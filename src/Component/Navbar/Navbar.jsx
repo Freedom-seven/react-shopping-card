68fb20b5a9b05095112f889c07f2668fe9e7606e
FREEDOM <freewhynane62@gmail.com>
2021-06-17 13:06:00 +0000

    add code to navbar component

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div className="container">
+            <div className="row navbar">
+                <div className="col-md-12">
+                    <h4 className="navbar-text sizes">Sizes:</h4>
+                    <p className="navbar-text found">16 Product(s) found</p>
+                    <p className="navbar-text order">Order by</p>
+                     <form className="navbar-text navbar-right text-end">
+                      <label for="select"></label>
+                      <select name="Select" id="Select">
+                          <option value="select">Select  </option>
+                      </select>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default Navbar;
